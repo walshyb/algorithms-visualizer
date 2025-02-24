5048b23267cdc247e21ae12992427729efcc5a5a
walshyb <bmwalshy@gmail.com>
2025-02-24 13:48:30 -0500

    Add blurb

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,35 @@ import TreeSearch from "./algorithms/TreeSearch";
 function App() {
   return (
     <main>
+      <p>
+        Welcome to my algorithm visualizer! This is a small project I am
+        creating to get some more hands on practice with C++ datastructures and
+        Wasm modules.
+      </p>
+
+      <p>
+        This is still a work in progress, but feel free to view{" "}
+        <a
+          href="https://github.com/walshyb/algorithms-visualizer?tab=readme-ov-file#roadmap"
+          target="_blank"
+          rel="noopener"
+        >
+          the roadmap
+        </a>
+        ,{" "}
+        <a
+          href="https://github.com/walshyb/algorithms-visualizer/issues"
+          target="_blank"
+          rel="noopener"
+        >
+          create an issue
+        </a>{" "}
+        if you notice anything weird, or check out some of{" "}
+        <a href="https://bwal.sh" target="_blank" rel="noopener">
+          my other projects
+        </a>
+      </p>
+
       {module && <BinarySearchArray />}
       {module && <TreeSearch strategy="Binary" target={3} />}
       {module && (
